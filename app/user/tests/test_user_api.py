5a7d21941a966f15bd5951023de0be67cf9e348a
sokhaiseang <saihok71@gmail.com>
2024-05-09 15:40:38 +0700

    implement create user API

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -59,5 +59,5 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
-        ).exists
+        ).exists()
         self.assertFalse(user_exists)
